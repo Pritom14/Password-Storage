b49df0485af7b48425d79aff5d3e6764e298ecb3
Akshay Nandwana <14etcit001@technonjr.org>
2017-12-19 11:14:29 +0530

    MainActivity design changed

--- app/src/main/java/com/nitsilchar/hp/passwordStorage/adapter/PasswordRecyclerViewAdapter.java
+++ app/src/main/java/com/nitsilchar/hp/passwordStorage/adapter/PasswordRecyclerViewAdapter.java
@@ -2,22 +2,23 @@ package com.nitsilchar.hp.passwordStorage.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterViewAnimator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nitsilchar.hp.passwordStorage.R;
 import com.nitsilchar.hp.passwordStorage.activity.DetailsActivity;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PasswordRecyclerViewAdapter extends RecyclerView.Adapter<PasswordRecyclerViewAdapter.ViewHolder> {
 
@@ -41,16 +42,33 @@ public class PasswordRecyclerViewAdapter extends RecyclerView.Adapter<PasswordRe
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.title.setText(accounts.get(position));
-        holder.title.setOnClickListener(new View.OnClickListener() {
+        holder.accountName.setText(accounts.get(position));
+        holder.description.setText("Description feature will add soon");
+        holder.iconText.setText(accounts.get(position).substring(0,1).toUpperCase());
+        holder.iconBg.setImageResource(R.drawable.bg_circle);
+        holder.iconBg.setColorFilter(getRandomMaterialColor());
+        holder.accountContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String data = accounts.get(position);
-                Intent intent=new Intent(context, DetailsActivity.class);
-                intent.putExtra("Site",data);
+                Intent intent = new Intent(context, DetailsActivity.class);
+                intent.putExtra("Site", data);
                 context.startActivity(intent);
             }
         });
+    }
+
+    private int getRandomMaterialColor() {
+        int returnColor = Color.GRAY;
+        int arrayId = context.getResources().getIdentifier("mdcolor_400", "array", context.getPackageName());
+
+        if (arrayId != 0) {
+            TypedArray colors = context.getResources().obtainTypedArray(arrayId);
+            int index = (int) (Math.random() * colors.length());
+            returnColor = colors.getColor(index, Color.GRAY);
+            colors.recycle();
+        }
+        return returnColor;
     }
 
     @Override
@@ -60,11 +78,20 @@ public class PasswordRecyclerViewAdapter extends RecyclerView.Adapter<PasswordRe
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView title;
+        @BindView(R.id.account_container)
+        LinearLayout accountContainer;
+        @BindView(R.id.account_name)
+        TextView accountName;
+        @BindView(R.id.description)
+        TextView description;
+        @BindView(R.id.icon_text)
+        TextView iconText;
+        @BindView(R.id.icon_bg)
+        ImageView iconBg;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            title = (TextView) itemView.findViewById(R.id.textview_title);
+            ButterKnife.bind(this,itemView);
         }
     }
 }
