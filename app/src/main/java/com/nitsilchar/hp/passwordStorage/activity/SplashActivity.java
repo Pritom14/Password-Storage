c82fc2d620c7d4c64e53ca710c37b437c466673b
Ishu Dohare <ishu.dohare.ece15@itbhu.ac.in>
2017-12-22 18:35:43 +0530

    Video Splash Added
    Splash Issue resolved

--- app/src/main/java/com/nitsilchar/hp/passwordStorage/activity/SplashActivity.java
+++ app/src/main/java/com/nitsilchar/hp/passwordStorage/activity/SplashActivity.java
@@ -1,5 +1,21 @@
 package com.nitsilchar.hp.passwordStorage.activity;
 
+/**
+ * Created by admin on 12/22/2017.
+ */
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.VideoView;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
@@ -18,24 +34,29 @@ import com.nitsilchar.hp.passwordStorage.activity.LoginRegistrationActivity;
 
 import io.fabric.sdk.android.Fabric;
 
+
 public class SplashActivity extends AppCompatActivity {
+
+    VideoView video;
+
     public static String str_login_test;
 
     public static SharedPreferences sh;
     public static SharedPreferences.Editor editor;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
 
-        //ADDED BY ThisIsNSH
-        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+
+        // getSupportActionBar().hide();
 
         sh = getSharedPreferences("myprefe", 0);
         editor = sh.edit();
@@ -46,14 +67,14 @@ public class SplashActivity extends AppCompatActivity {
             return;
         }
 
-        new Handler().postDelayed(new Runnable() {
-
+        video=(VideoView)findViewById(R.id.videoView);
+        Uri videouri=Uri.parse("android.resource://" +getPackageName()+"/"+R.raw.splash_final);//location of the splash video
+        video.setVideoURI(videouri);
+        video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void run() {
-                /*
-                 * if user login test is true on oncreate then redirect the user
-                 * to result page
-                 */
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                // action which happens after completion of the video
+                //*****************************************//
 
                 if (str_login_test != null
                         && !str_login_test.toString().trim().equals("")) {
@@ -72,15 +93,50 @@ public class SplashActivity extends AppCompatActivity {
                     startActivity(send);
 
                 }
-            }
 
-        }, 3000);
+                //*****************************************************************//
+
+            }
+        });
+
+        video.start();
+
+        //if the user touches the screen while splash screen video is playing, it moves to the next activity as deignated
+
+        video.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                ////////////////////////////////////////////////////////////
+                if (str_login_test != null
+                        && !str_login_test.toString().trim().equals("")) {
+                    Intent send = new Intent(getApplicationContext(),
+                            LoginActivity.class);
+                    startActivity(send);
+                }
+                /*
+                 * if user login test is false on oncreate then redirect the
+                 * user to login & registration page
+                 */
+                else {
+
+                    Intent send = new Intent(getApplicationContext(),
+                            LoginRegistrationActivity.class);
+                    startActivity(send);
+
+                }
+                ////////////////////////////////////////////////////////////////
+               // finish();
+                return false;
+            }
+        });
+
+
 
     }
 
     public boolean containsPass(String str){
 
-       return  sh.contains(str)?true:false;
+        return  sh.contains(str)?true:false;
 
 
     }
